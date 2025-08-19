156eeb0a398224fd93fc8f7f351dc8d58b89667c
unknown <arifk62901@gmail.com>
2025-08-19 20:11:43 +0600

    added navbar

---
+++ client/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+const inter = Inter({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Learning Management System",
+  description: "this is Learning Management system platform",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased ` } cz-shortcut-listen="true">{children}</body>
+    </html>
+  );
+}
---
+++ client/app/page.tsx
@@ -0,0 +1,7 @@
+import Navbar from "@/components/shared/navbar";
+
+export default function Home() {
+  return <div>
+   <Navbar />
+  </div>;
+}
